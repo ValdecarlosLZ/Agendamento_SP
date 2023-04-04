7ccda88f28777b6fea518bd0e8b7c587e6853b30
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-04-04 10:30:32 -0400

    Create conclusao.php

---
+++ conclusao.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$name = $_GET['name'];
+$last_name = $_GET['last_name'];
+$email = $_GET['email'];
+$cpf = $_GET['cpf'];
+$rg = $_GET['rg'];
+$inicio = $_GET['inicio'];
+$fim = $_GET['fim'];
+$quarto = $_GET['quarto'];
+$pessoa = $_GET['pessoa'];
+?>
