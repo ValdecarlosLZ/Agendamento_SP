43e31adec6718f819b3e3c883e61e4502faf73a1
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-04-04 21:01:10 -0400

    Update quartos.php

--- quartos.php
+++ quartos.php
@@ -1,6 +1,24 @@
 <?php
 session_start();
 
+$quarto1 = $_POST['quarto1'];
+$quarto2 = $_POST['quarto2'];
+$quarto3 = $_POST['quarto3'];
+$conclusao = $_POST['conclusao'];
+
+if ($quarto1 = true) {
+    header('Location: ');
+    $conclusao = header('Location: Cadastro_efetuado.html');
+}
+if ($quarto2 = true){
+    header('Location: ');
+    $conclusao = header('Location: Cadastro_efetuado.html');
+}
+if($quarto3 = true){
+    header('Location: ');
+    $conclusao = header('Location: Cadastro_efetuado.html');
+}
+
 
 
 ?>
