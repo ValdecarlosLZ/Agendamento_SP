0b2530167aef7bfec0816b0fbe38ab68f4b86a97
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-04-11 08:12:59 -0400

    .

--- quartos.php
+++ quartos.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 
-$quarto1 = $_POST['quarto1'];
-$quarto2 = $_POST['quarto2'];
-$quarto3 = $_POST['quarto3'];
+
 $QT1 = $_POST['QT1'];
 $QT2 = $_POST['QT2'];
 $QT3 = $_POST['QT3'];
