87e47b980c900797673f6cef3f3b7dead875544e
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-04-14 08:48:35 -0400

    .

--- conclusao.php
+++ conclusao.php
@@ -18,22 +18,6 @@ $QT3 = $_SESSION['QT3'];
 $conclusao = $_SESSION['conclusao'];
 
 
-// @$name = $_GET['name'];
-// @$last_name = $_GET['last_name'];
-// @$email = $_GET['email'];
-// @$cpf = $_GET['cpf'];
-// @$rg = $_GET['rg'];
-// @$inicio = $_GET['inicio'];
-// @$fim = $_GET['fim'];
-// @$pessoa = $_GET['pessoa'];
-// @$quarto1 = $_GET['quarto1'];
-// @$QT1 = $_GET['QT1'];
-// @$quarto2 = $_GET['quarto2'];
-// @$QT2 = $_GET['QT2'];
-// @$quarto3 = $_GET['quarto3'];
-// @$QT3 = $_GET['QT3'];
-// @$conclusao = $_GET['conclusao'];
-
 if ($quarto1 = true) {
     echo $name;
     echo $last_name;
