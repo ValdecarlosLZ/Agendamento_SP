31933dcad06f8e0e1fc905ac183a82a83f94dc73
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-05-04 21:27:01 -0400

    Update agendamento.php

--- agendamento.php
+++ agendamento.php
@@ -1,28 +1,34 @@
 <?php
 session_start();
 
+include "conexao.php";
+
+
 //Leitura de dados
 
 $name = $_POST['name'];
+
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
 $cpf = $_POST['cpf'];
 $rg = $_POST['rg'];
-$tempo = $_POST['tempo'];
 
 
-if(($email != "") && ($cpf >= 11) && ($rg >= 6)){
+$nome_completo = $name." ".$last_name;
+
+
+$sql = "INSERT INTO Dados_pessoais (Nome_completo,Email,Cpf,Rg) VALUES (:nome_completo,:email,:cpf,:rg)";
+
+$stmt = $conexao->prepare($sql);
+
+$stmt->bindValue(':nome_completo',$nome_completo);
+$stmt->bindValue(':email',$email);
+$stmt->bindValue(':cpf',$cpf);
+$stmt->bindValue(':rg',$rg);
+
+
+$stmt->execute();
+
     header('Location: cadastro2.html');
-}else{ 
-header('Location: index.html?Erro');
-}
-$_SESSION ['name'] = $name;
-$_SESSION ['last_name'] = $last_name;
-$_SESSION ['email'] = $email;
-$_SESSION ['cpf'] = $cpf;
-$_SESSION ['rg'] = $rg;
-$_SESSION ['tempo'] = $tempo;
-
-
-?>
-
+    exit;
+    ?>
