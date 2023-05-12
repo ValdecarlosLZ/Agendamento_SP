396b7e928690ab08822224f95b411e5a16ee7931
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-05-11 20:32:17 -0400

    Update quartos.php

--- quartos.php
+++ quartos.php
@@ -1,36 +1,12 @@
 <?php
-session_start();
+include "conexao.php";
 
-$quarto1 = $_POST ['quarto1'];
-$quarto2 = $_POST ['quarto2'];
-$quarto3 = $_POST ['quarto3'];
-$QT1 = $_POST['QT1'];
-$QT2 = $_POST['QT2'];
-$QT3 = $_POST['QT3'];
-$conclusao = $_POST['conclusao'];
+$buscar_cadastro = "SELECT * FROM quartos";
 
-if ($quarto1 = true) {
-    $conclusão = 100 * $QT1;
-    header('Location: Cadastro_efetuado.html');
-    
-}
-if ($quarto2 = true){
-    $conclusão = 150 * $QT2;
-    header('Location: Cadastro_efetuado.html');
-}
-if($quarto3 = true){
-    $conclusao = 200 * $QT3;
-    header('Location: Cadastro_efetuado.html');
-    
-}
+$stmt = $conexao->prepare($buscar_cadastro);
 
-$_SESSION ['quarto1'] = $quarto1;
-$_SESSION ['quarto2'] = $quarto2;
-$_SESSION ['quarto3'] = $quarto3;
-$_SESSION ['QT1'] = $QT1;
-$_SESSION ['QT2'] = $QT2;
-$_SESSION ['QT3'] = $QT3;
-$_SESSION ['conclusao'] = $conclusao;
+$stmt->execute();
 
+$receber_cadastro = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
